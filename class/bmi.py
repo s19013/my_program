cdfe9bc10376130195d62e7732d3ce66077f603b
s19013 <s19013@std.it-college.ac.jp>
2019-07-30 14:12:19 +0900

    1

---
+++ class/bmi.py
@@ -0,0 +1,27 @@
+#bmi 計算
+class BMI:
+    def __init__(self,weight,height):
+        """初期化"""
+        self.weight=weight
+        self.height=height
+        self.calcBMI()
+
+    def calcBMI(self):
+        """計算"""
+        h=self.height/100
+        self.bmi=self.weight/(h**2)
+
+    def printjudge(self):
+        """結果"""
+    #後でreturnにかえる
+        print("---")
+        print("bmi=",self.bmi)
+        b=self.bmi
+        if (b<18.5): print("痩せ")
+        elif (b<25):print("標準")
+        elif(b<30):print("肥満（軽）")
+        else:print("肥満（重）")
+
+
+p1=BMI(weight=65,height=165)
+p1.printjudge()
