9c76fd21810b5e4b03ce8ae309c1904ca90f7c45
s19013 <s19013@std.it-college.ac.jp>
2019-07-30 14:57:41 +0900

    1

--- 1.py
+++ 1.py
@@ -1,15 +1,3 @@
-import re
-
-def is_phone_number(text):
-    if len(text)!=
-
-message=""
-for i in range(len(message)):
-    chunk=message[i:i+12]
-    if is_phone_number(chunk):
-        print(""+chunk)
-print("")
-
-phone_number_regex=re.compiler(r'\d\d\d-\d\d\d-\d\d\d\d')
-mo =phone_number_regex.serch(message)
-print(""+mo.group())
+box=[1,2,3,4]
+for i in range(len(box)):
+    print(i)
--- class/bmi.py
+++ class/bmi.py
@@ -1,7 +1,10 @@
 #bmi 計算
+box_weight=[]
+box_height=[]
 class BMI:
-    def __init__(self,weight,height):
+    def __init__(self,person,weight,height):
         """初期化"""
+        self.person=person
         self.weight=weight
         self.height=height
         self.calcBMI()
@@ -10,18 +13,37 @@ class BMI:
         """計算"""
         h=self.height/100
         self.bmi=self.weight/(h**2)
+        self.printjudge()
 
     def printjudge(self):
         """結果"""
     #後でreturnにかえる
         print("---")
-        print("bmi=",self.bmi)
+        print("person{}\nbmi={}".format(self.person,self.bmi))
         b=self.bmi
         if (b<18.5): print("痩せ")
         elif (b<25):print("標準")
         elif(b<30):print("肥満（軽）")
         else:print("肥満（重）")
 
+def input_person():
+    #t eは省略
+    while True:
+        weight=input("体重：")
+        if weight=="q":
+            break
+        else:weight=int(weight)
+        height=int(input("身長："))
+        box_weight.append(weight)
+        box_height.append(height)
 
-p1=BMI(weight=65,height=165)
-p1.printjudge()
+
+
+    main()
+
+def main():
+    for (p,w,h) in zip(range(1,len(box_weight)+1),box_weight,box_height):
+        BMI(p,w,h)
+
+
+input_person()
