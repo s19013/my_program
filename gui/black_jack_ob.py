933d086753a2c843c5afa8d2693b7561f41fa39f
s19013 <s19013@std.it-college.ac.jp>
2019-08-01 14:50:02 +0900

    1

--- gui/black_jack_ob.py
+++ gui/black_jack_ob.py
@@ -7,8 +7,37 @@ from tkinter import font
 import tkinter as tk
 import tkinter.ttk as ttk
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        
+root = tk.Tk().title("Visualizer for function")
+strfont=font.Font(size=25)
+framefont=font.Font(size=16)
 
-    def create_widgets(self):
+
+class Application(tk.Frame):
+    def __init__(self, master = None):
+        tk.Frame.__init__(self, master)
+        self.pack(expand = 1, fill = tk.BOTH, anchor = tk.NW)
+        #フレーム
+        self.comside=tk.LabelFrame(win,bd=3,relief="groove",text="相手",font=framefont)
+        self.comside.pack(fill="x")
+
+        self.etcside=tk.Frame(win)
+        self.etcside.pack(fill="x")
+
+        self.yourside=tk.LabelFrame(win,bd=3,relief="groove",text="あなた",font=framefont)
+        self.yourside.pack(fill="x")
+
+class Card_base:
+    def __init__(self,parson):
+        self.parson=parson
+        self.parsoncard=[]
+        for i in range(2):
+            self.parsoncard.append(random.randint(1,13))
+
+    def hyouzi(self,parson):
+        self.parson=parson
+        self.parsoncard=tk.StringVar()
+        self.parsoncard.set("{}".format(self.parsoncard))
+        self.textparson_sum=tk.StringVar()
+        self.textparson_sum.set("あなたの合計:{}".format(sum(self.parsoncard)))
+        lmycord=tk.Label(yourside,font=strfont,textvariable=textmycard).pack(side"left")
+        lmycord_sum=tk.Label(yourside,font=strfont,textvariable=textmycard_sum).pck(side="right")
