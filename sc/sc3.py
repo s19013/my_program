7a0f58e00393c71b643b5e83b2c72dc499033b59
s19013 <s19013@std.it-college.ac.jp>
2019-08-09 01:38:32 +0900

    1

--- sc/sc3.py
+++ sc/sc3.py
@@ -5,10 +5,7 @@ r = requests.get("https://www26.atwiki.jp/gcmatome/pages/24.html")
 
 soup = BeautifulSoup(r.content, "html.parser")
 
-with open(soup) as f:
-    lines = f.readlines()
-
-lines_strip = [line.strip() for line in lines]
+text=
 result = [line for line in lines_strip if 'title' in line]
-for line in result:
-    print(line)
+for a in result:
+    print(a)
---
+++ tramp_card.py
@@ -0,0 +1,12 @@
+a=list(range(4,56))
+box=[]
+for b in a:
+    box.append(divmod(b,4))
+
+print(box)
+#(d,f)
+#d=数字
+#f=0　スペード
+#f=1　ハート
+#f=2　ダイヤ
+#f=3　クラブ
