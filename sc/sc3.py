61f31db6f7cbb836bd36dfb4dd1065f4d8423eb5
s19013 <s19013@std.it-college.ac.jp>
2019-08-09 23:07:50 +0900

    1

--- sc/sc3.py
+++ sc/sc3.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 r = requests.get("https://www26.atwiki.jp/gcmatome/pages/24.html")
 
 soup = BeautifulSoup(r.content, "html.parser")
+l=soup.find_all("a")
+with open(r,"w")
 
-text=
-result = [line for line in lines_strip if 'title' in line]
-for a in result:
-    print(a)
+#result = [line for line in lines_strip if 'title' in line]
+#for a in result:
+    #print(a)
