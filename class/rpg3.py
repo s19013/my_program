5ee0cfe99940b6d5d25f5a4e631bf453efb2b7a3
s19013 <s19013@std.it-college.ac.jp>
2019-08-15 00:58:19 +0900

    1

--- class/rpg3.py
+++ class/rpg3.py
@@ -1,20 +1,29 @@
 import random
 class human:
     """大体共通するクラス"""
-    def __init__(self,name,hp,mp):
+    def __init__(self,name,hp,mp,health):
         self.name=name
         self.hp=hp
         self.mp=mp
-
+        self.health=health
+#ステータスを取る
     def getname(self):
         return self.name
     def gethp(self):
         return self.hp
     def getmp(self):
         return self.mp
-
-    def receive_damage(self,damage):
+##hp mpの変動
+#減る
+    def remaing_hp(self,damage):
         self.hp-=damage
+    def remaing_mp(self,used_mp):
+        self.mp-=used_mp
+#増える
+    def charge_hp(self,care_hp):
+        self.hp+=care_hp
+    def charge_mp(self,care_mp):
+        self.mp+=care_mp
 
         #後々調整
     def base_attack(self,trick):
@@ -36,20 +45,9 @@ class human:
     #     return self.base*cri
 
 
-    def remaing_mp(self,used_mp):
-        self.used_mp=used_mp
-        self.mp-=used_mp
-        return self.mp
 
-    def charge_hp(self,care_hp):
-        self.care_hp=care_hp
-        self.hp+=care_hp
-        return self.hp
 
-    def charge_mp(self,care_mp):
-        self.care_mp=care_mp
-        self.mp+=care_mp
-        return self.mp
+
 
 class info:
     def show_info(self,name,hp,mp):
@@ -60,7 +58,9 @@ class info:
 HP:{}
 MP:{}""".format(self.name,self.hp,self.mp))
 class man(human):
-    pass
-man2=man("man",100,100)
+    def special(self):
+        speciallist=[1:""]
+
+man2=man("man",100,100,good)
 #woman=human("woman",90,110)
 #info().show_info(man.getname(),man.gethp(),man.getmp())
