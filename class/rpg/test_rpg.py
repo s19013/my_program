297f5a1a33acd84c2b7a338fd9a36b945f401951
s19013 <s19013@std.it-college.ac.jp>
2019-08-22 10:13:58 +0900

    1

--- class/rpg/test_rpg.py
+++ class/rpg/test_rpg.py
@@ -5,11 +5,11 @@
 #強化は正の数
 #弱体は負の数
 #
-import rpg3
-rpg3.man2.next_do()
-rpg3.man2.next_do()
-rpg3.man2.next_do()
-rpg3.man2.next_do()
-rpg3.man2.next_do()
-rpg3.man2.next_do()
-rpg3.man2.next_do()
+import hero
+hero.man2.next_do()
+hero.man2.next_do()
+hero.man2.next_do()
+hero.man2.next_do()
+hero.man2.next_do()
+hero.man2.next_do()
+hero.man2.next_do()
