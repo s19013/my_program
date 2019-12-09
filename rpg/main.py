9b2cf120711e6a3d6e8c95f06c2629a7bafb3566
s19013 <s19013@std.it-college.ac.jp>
2019-12-10 01:29:46 +0900

    1

--- rpg/main.py
+++ rpg/main.py
@@ -1,1 +1,14 @@
-import
+# import hero_base
+# import enemy_base
+from hero_base import Human
+from enemy_base import Slime
+
+class Main:
+    def criate_crature(self):
+        self.hum = hero_base.Human("man","debug",90,100,100)
+        self.ene = enemy_base.Slime("ene","debug",90,100,100)
+
+    def decide_order(self):
+        self.heros_list=[hum]
+        self.enemy_list=[ene]
+        self.orderlist = [self.hum,self.ene]
