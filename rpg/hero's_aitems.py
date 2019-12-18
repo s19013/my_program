1348a7fc0d762919628a27ce11e86aa0ee4e3c37
s19013 <s19013@std.it-college.ac.jp>
2019-12-19 01:59:02 +0900

    1

---
+++ rpg/hero's_aitems.py
@@ -0,0 +1,17 @@
+class Item:
+    h=10
+    h_ex = 5
+    h_S = 2
+    m =10
+    m_ex = 5
+    m_s = 2
+    au = 10
+    du = 10
+    cu = 10
+    anti_p = 10
+    oukyu = 10
+    re = 10
+    re_ex = 5
+    re_S = 2
+
+    
